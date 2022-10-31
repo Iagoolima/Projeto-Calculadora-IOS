93675e22a5ce874309473ea0bbb9f7dead877497
iagoolima <iagosilvalima22@outlook.com>
2022-10-31 15:20:40 -0300

    remoção comentarios

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 function insert(num){
     let number = document.getElementById('resultNumber').innerHTML
     document.getElementById('resultNumber').innerHTML = number + num
-
-
-    //teste
-
-    //teste2
-
-    //teste3
-
-    //teste4
-
-    //teste5
 }
